8ff3f63794f67fdc827c248d934200295c5b7f46
Amit Shukla <ashuklax@gmail.com>
2019-04-29 12:36:32 -0700

    Fixed Typo
    Fixed Typo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,7 +22,7 @@ export const environment = {
     storageBucket: "",
     messagingSenderId: ""
   }
-};
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
